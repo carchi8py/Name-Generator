8515b76f95d8c4f932213bf481deccb005e12426
Chris Archibald <carchi8py@gmail.com>
2016-09-03 10:59:46 -0700

    Added user input to our code

--- generator.py
+++ generator.py
@@ -1,11 +1,25 @@
 import random
 import string
 
-def generator():
-    letter1 = random.choice(string.ascii_lowercase)
-    letter2 = random.choice(string.ascii_lowercase)
-    letter3 = random.choice(string.ascii_lowercase)
-    name = letter1 + letter2 + letter3
-    return name
+vowels = 'aeiou'
+consonants = 'bcdfghjklmnpqrstvwxyz'
+all = vowels + consonants
+message = "What letter do you want? Enter 'v' for vowels, 'c' for consonants, 'l' for any letter: "
 
-print generator()
+letter_input_1 = raw_input(message)
+letter_input_2 = raw_input(message)
+letter_input_3 = raw_input(message)
+
+def generator(input_letter):
+    if input_letter == 'v':
+        out_letter = random.choice(vowels)
+    elif input_letter == 'c':
+        out_letter = random.choice(consonants)
+    elif input_letter:
+        out_letter = random.choice(all)
+    else:
+        out_letter = letter_input_1
+    return out_letter
+
+name = generator(letter_input_1) + generator(letter_input_2) + generator(letter_input_3)
+print name
